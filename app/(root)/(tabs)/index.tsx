263e8891bb8482b8660ac7769a026e6e7489b98a
gdev-kingx <marioplayz975@gmail.com>
2025-03-06 18:31:50 +0530

    init

--- app/(root)/(tabs)/index.tsx
+++ app/(root)/(tabs)/index.tsx
@@ -1,11 +1,21 @@
 import {View, Text} from 'react-native'
-import React from 'react'
+import {Link} from "expo-router";
 
 const Index = () => {
 	return (
-		<View>
-			<Text>Index</Text>
+		<View
+			style={{
+				flex: 1,
+				justifyContent: "center",
+				alignItems: "center",
+			}}
+		>
+			<Link href={"/sign-in"}>Sign-In</Link>
+			<Link href={"/explore"}>Explore</Link>
+			<Link href={"/profile"}>Profile</Link>
+			<Link href={"/properties/1"}>Property</Link>
 		</View>
 	)
 }
+
 export default Index
